483aa29491cfcee954a676efa44783395815108f
Petre Popescu <petre.popescu@hostway.com>
2015-12-29 14:16:32 -0600

    Layout issues resolved

--- app/src/main/java/com/qualimony/alcal/MainActivity.java
+++ app/src/main/java/com/qualimony/alcal/MainActivity.java
@@ -15,6 +15,7 @@ import android.view.MenuItem;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.Space;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -52,17 +53,17 @@ public class MainActivity extends AppCompatActivity {
 
         TableLayout layout = (TableLayout)findViewById(R.id.tableLayout);
         layout.setStretchAllColumns(true);
+        layout.setShrinkAllColumns(true);
 
         //First row
         TableRow row = new TableRow(this);
-        TableRow.LayoutParams rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 1);
-        row.setLayoutParams(rowParams);
-        //row.setPadding(0, 5, 0, 5);
+        TableRow.LayoutParams rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT, 1);
+        //row.setLayoutParams(rowParams);
 
         Button currentDate = new Button(this);
         currentDate.setTypeface(face);
         currentDate.setText(fullFormat.format(today.getTimeInMillis()));
-        rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 1);
+        rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT, 0);
         rowParams.column = 0;
         rowParams.span = 8;
         rowParams.gravity = Gravity.CENTER;
@@ -72,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
         TextView v = new TextView(this);
         v.setTypeface(face);
         v.setText("\u263d");
-        rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 1);
+        rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT, 0);
         rowParams.column = 8;
         rowParams.span = 1;
         rowParams.gravity = Gravity.CENTER;
@@ -81,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
         v = new TextView(this);
         v.setTypeface(face);
         v.setText("\u2642");
-        rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 1);
+        rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT, 0);
         rowParams.column = 9;
         rowParams.span = 1;
         rowParams.gravity = Gravity.CENTER;
@@ -90,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
         v = new TextView(this);
         v.setTypeface(face);
         v.setText("\u263f");
-        rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 1);
+        rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT, 0);
         rowParams.column = 10;
         rowParams.span = 1;
         rowParams.gravity = Gravity.CENTER;
@@ -99,7 +100,7 @@ public class MainActivity extends AppCompatActivity {
         v = new TextView(this);
         v.setTypeface(face);
         v.setText("\u2643");
-        rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 1);
+        rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT, 0);
         rowParams.column = 11;
         rowParams.span = 1;
         rowParams.gravity = Gravity.CENTER;
@@ -108,7 +109,7 @@ public class MainActivity extends AppCompatActivity {
         v = new TextView(this);
         v.setTypeface(face);
         v.setText("\u2640");
-        rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 1);
+        rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT, 0);
         rowParams.column = 12;
         rowParams.span = 1;
         rowParams.gravity = Gravity.CENTER;
@@ -117,7 +118,7 @@ public class MainActivity extends AppCompatActivity {
         v = new TextView(this);
         v.setTypeface(face);
         v.setText("\u2644");
-        rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 1);
+        rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT, 0);
         rowParams.column = 13;
         rowParams.span = 1;
         rowParams.gravity = Gravity.CENTER;
@@ -126,7 +127,7 @@ public class MainActivity extends AppCompatActivity {
         v = new TextView(this);
         v.setTypeface(face);
         v.setText("\u2609");
-        rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 1);
+        rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT, 0);
         rowParams.column = 14;
         rowParams.span = 1;
         rowParams.gravity = Gravity.CENTER;
@@ -136,14 +137,16 @@ public class MainActivity extends AppCompatActivity {
 
         //Second row
         row = new TableRow(this);
-        rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 1);
-        row.setLayoutParams(rowParams);
-        //row.setPadding(0, 5, 0, 5);
+        rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT, 0);
+        //row.setLayoutParams(rowParams);
 
         TextView selectedMonth = new TextView(this);
         selectedMonth.setTypeface(face);
         selectedMonth.setText(monthFormat.format(today.getTimeInMillis()));
-        rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 1);
+        selectedMonth.setTextSize(16);
+        selectedMonth.setMaxLines(1);
+        selectedMonth.setWidth(200);
+        rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT, 0);
         rowParams.column = 0;
         rowParams.span = 8;
         rowParams.gravity = Gravity.CENTER;
@@ -155,14 +158,13 @@ public class MainActivity extends AppCompatActivity {
 
         //Third row
         row = new TableRow(this);
-        rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 1);
-        row.setLayoutParams(rowParams);
-        //row.setPadding(0, 5, 0, 5);
+        rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT, 0);
+        //row.setLayoutParams(rowParams);
 
         v = new Button(this);
         v.setTypeface(face);
         v.setText("<");
-        rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 1);
+        rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT, 0);
         rowParams.column = 0;
         rowParams.span = 4;
         rowParams.gravity = Gravity.CENTER;
@@ -171,7 +173,7 @@ public class MainActivity extends AppCompatActivity {
         v = new Button(this);
         v.setTypeface(face);
         v.setText(">");
-        rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 1);
+        rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT, 0);
         rowParams.column = 4;
         rowParams.span = 4;
         rowParams.gravity = Gravity.CENTER;
@@ -184,98 +186,12 @@ public class MainActivity extends AppCompatActivity {
         //fourth, fifth row
         for(int j = 0; j < 2; j++) {
             row = new TableRow(this);
-            rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 1);
-            row.setLayoutParams(rowParams);
-            //row.setPadding(0, 5, 0, 5);
+            rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 0);
+            //row.setLayoutParams(rowParams);
             addRowForWeek(row);
             layout.addView(row);
         }
 
-
-
-        /*
-        GridLayout layout = (GridLayout)findViewById(R.id.gridLayout);
-
-        TextView selectedMonth = new TextView(this);
-        selectedMonth.setTypeface(face);
-        selectedMonth.setText("364.28.\u2650");
-        GridLayout.LayoutParams params = new GridLayout.LayoutParams();
-        params.rowSpec = GridLayout.spec(0, 1, GridLayout.CENTER);
-        params.columnSpec = GridLayout.spec(0, 8, GridLayout.CENTER);
-        layout.addView(selectedMonth, params);
-
-        TextView previous = new TextView(this);
-        previous.setTypeface(face);
-        previous.setText("<");
-        params = new GridLayout.LayoutParams();
-        params.rowSpec = GridLayout.spec(1, 1, GridLayout.CENTER);
-        params.columnSpec = GridLayout.spec(0, 4, GridLayout.CENTER);
-        layout.addView(previous, params);
-
-        TextView next = new TextView(this);
-        next.setTypeface(face);
-        next.setText(">");
-        params = new GridLayout.LayoutParams();
-        params.rowSpec = GridLayout.spec(1, 1, GridLayout.CENTER);
-        params.columnSpec = GridLayout.spec(4, 4, GridLayout.CENTER);
-        layout.addView(next, params);
-
-        TextView mo = new TextView(this);
-        mo.setTypeface(face);
-        mo.setText("\u263d");
-        params = new GridLayout.LayoutParams();
-        params.rowSpec = GridLayout.spec(0, 1, GridLayout.CENTER);
-        params.columnSpec = GridLayout.spec(9, 1, GridLayout.CENTER);
-        layout.addView(mo, params);
-
-        TextView tu = new TextView(this);
-        tu.setTypeface(face);
-        tu.setText("\u2642");
-        params = new GridLayout.LayoutParams();
-        params.rowSpec = GridLayout.spec(0, 1, GridLayout.CENTER);
-        params.columnSpec = GridLayout.spec(10, 1, GridLayout.CENTER);
-        layout.addView(tu, params);
-
-        TextView we = new TextView(this);
-        we.setTypeface(face);
-        we.setText("\u263f");
-        params = new GridLayout.LayoutParams();
-        params.rowSpec = GridLayout.spec(0, 1, GridLayout.CENTER);
-        params.columnSpec = GridLayout.spec(11, 1, GridLayout.CENTER);
-        layout.addView(we, params);
-
-        TextView th = new TextView(this);
-        th.setTypeface(face);
-        th.setText("\u2643");
-        params = new GridLayout.LayoutParams();
-        params.rowSpec = GridLayout.spec(0, 1, GridLayout.CENTER);
-        params.columnSpec = GridLayout.spec(12, 1, GridLayout.CENTER);
-        layout.addView(th, params);
-
-        TextView fr = new TextView(this);
-        fr.setTypeface(face);
-        fr.setText("\u2640");
-        params = new GridLayout.LayoutParams();
-        params.rowSpec = GridLayout.spec(0, 1, GridLayout.CENTER);
-        params.columnSpec = GridLayout.spec(13, 1, GridLayout.CENTER);
-        layout.addView(fr, params);
-
-        TextView sa = new TextView(this);
-        sa.setTypeface(face);
-        sa.setText("\u2644");
-        params = new GridLayout.LayoutParams();
-        params.rowSpec = GridLayout.spec(0, 1, GridLayout.CENTER);
-        params.columnSpec = GridLayout.spec(14, 1, GridLayout.CENTER);
-        layout.addView(sa, params);
-
-        TextView su = new TextView(this);
-        su.setTypeface(face);
-        su.setText("\u2609");
-        params = new GridLayout.LayoutParams();
-        params.rowSpec = GridLayout.spec(0, 1, GridLayout.CENTER);
-        params.columnSpec = GridLayout.spec(15, 1, GridLayout.CENTER);
-        layout.addView(su, params);
-        */
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -294,13 +210,14 @@ public class MainActivity extends AppCompatActivity {
         for(int i = 0; i < 7; i++) {
             Button v = new Button(this);
             v.setBackgroundColor(0xffff0000);
-            TableRow.LayoutParams rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 1);
+            v.setHighlightColor(0xff00ff00);
+            TableRow.LayoutParams rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT, 0);
             rowParams.column = 8+i;
-            rowParams.span = 1;
+            //rowParams.span = 1;
             rowParams.gravity = Gravity.CENTER;
             rowParams.width = 50;
-            rowParams.leftMargin = 2;
-            rowParams.rightMargin = 2;
+            rowParams.leftMargin = 1;
+            rowParams.rightMargin = 1;
             rowParams.topMargin = 2;
             rowParams.bottomMargin = 2;
             row.addView(v, rowParams);
